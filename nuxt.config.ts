69eceba98abc61f3b460babbb9f7cbda9a6d38c3
ollatomiwa <ajaomoshood@gmail.com>
2025-03-17 20:42:51 +0100

    nuxt img installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-01-01',  // Updated to valid compatibility date
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt'
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/image'],
   vite: {
     optimizeDeps: {
       include: ['date-fns-tz']
