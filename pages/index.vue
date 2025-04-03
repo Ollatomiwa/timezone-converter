476756f166aa05d3deddaa80da346aa5e0841bf9
ollatomiwa <ajaomoshood@gmail.com>
2025-04-03 11:48:41 +0100

    update to homepage UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,45 @@
 <template>
   <div>
     <main>
-      <section class="bg-[#80361B]">
-        <div
-          class="container flex flex-col lg:flex-row items-center lg:py-20 py-10 gap-10"
-        >
-          <div
-            class="flex-1 sm:order-1 lg:order-1 text-center lg:text-left lg:ml-10 md:mr-6"
-          >
+      <section class="bg-[#80361B] py-[70px] text-center">
+        <div class="row justify-content-md-center">
+          <div class="col col-md-8">
             <h1
-              class="text-2xl lg:text-8xl font-extrabold mb-6 text-white"
+              class="text-2xl lg:text-8xl font-extrabold mb-1 text-white"
               ref="herotext"
             >
               <span class="text-orange-400">TIMEZONE </span>
               <span class="text-dodgeroll-gold">CONVERTER</span>
             </h1>
-            <p class="text-xl lg:text-2xl mb-8 text-white" ref="subtext">
+            <p class="text-[15px] lg:text-xl mb-5 text-white" ref="subtext">
               FIX TEAM MEETINGS WITH MEMBERS ON DIFFERENT TIME ZONES ON
-              APPROPRIATE TIME FOR ALL...
+              APPROPRIATE TIME FOR ALL
             </p>
             <NuxtLink
               to="/converter"
-              class="bg-orange-400 text-white px-6 py-3 rounded-full font-semibold hover:bg-dodgeroll-gold transition duration-900 inline-block"
+              class="bg-orange-400  text-white px-6 py-3 rounded-full font-semibold hover:bg-dodgeroll-gold transition duration-900 inline-block"
             >
-              GET STARTED <Icon name="material-symbols:arrow-circle-right" class="bg-white size-3" />
+              GET STARTED
+              <Icon
+                name="material-symbols:arrow-circle-right"
+                class="bg-white size-3"
+              />
             </NuxtLink>
           </div>
-          <div class="flex-1 order-2 lg:order-2 hidden lg:block">
+          <div class="flex mt-6 justify-center items-center gap-4  md:block">
             <NuxtImg
               sizes="xs:100vw sm:900px"
-              src="hero-brown2.png"
+              src="hero3.png"
               format="webp"
               alt="Hero Page"
-              densities="x1"
+              class="inline-block"
+            ></NuxtImg>
+            <NuxtImg
+              sizes="xs:100vw sm:900px"
+              src="hero4.png"
+              format="webp"
+              alt="Hero Page"
+              class="inline-block"
             ></NuxtImg>
           </div>
         </div>
@@ -40,6 +47,6 @@
     </main>
   </div>
   <About />
-  <hr>
+  <hr />
   <Newsletter />
 </template>
